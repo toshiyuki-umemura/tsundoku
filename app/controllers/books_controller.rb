2d199773a756727c3507eec6ef922ae84c9e2650
chokobo-ume <@users.noreply.github.com>
2020-02-08 15:08:20 +0900

    Update books_controller.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,17 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
+    @book.tags.build
   end
 
   def create
+    @book = current_user.books.create(book_params)
   end
+
+  private
+  def book_params
+    params.require(:book).permit( :title, :content, tags_attributes: [:id, :book_id, :tag])
+  end
+
 end
