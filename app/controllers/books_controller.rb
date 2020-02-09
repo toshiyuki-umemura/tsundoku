2e992c70192b817b9ebffe9fd6105ef4b50b6a76
chokobo-ume <@users.noreply.github.com>
2020-02-09 12:58:54 +0900

    Update application.html.erb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,11 +11,6 @@ class BooksController < ApplicationController
   def create
     @book = current_user.books.create(book_params)
   end
-
-  private
-  def book_params
-    params.require(:book).permit( :title, :content, tags_attributes: [:id, :book_id, :tag])
-  end
   
   def destroy
     book = Book.find(params[:id])
@@ -26,6 +21,11 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+  end
+
+  private
+  def book_params
+    params.require(:book).permit( :title, :content, tags_attributes: [:id, :book_id, :tag])
   end
 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :comments
-  has_many :tags
+  has_many :tags, dependent: :destroy
   accepts_nested_attributes_for :tags
 end
