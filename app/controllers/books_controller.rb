401e9b3bfbc0808ded558435a5127aacddcaf491
chokobo-ume <@users.noreply.github.com>
2020-02-11 20:13:05 +0900

    Update books_controller.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,12 +10,37 @@ class BooksController < ApplicationController
 
   def create
     @book = current_user.books.create(book_params)
+
+    flash[:notice] = "投稿しました"
+    redirect_to root_path
   end
-  
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.user_id == current_user.id
+      @book.update(book_params)
+
+      flash[:notice] = "投稿を更新しました"
+      redirect_to root_path
+    else
+      render :edit
+      binding.pry
+    end
+  end
+
   def destroy
     book = Book.find(params[:id])
     if book.user_id == current_user.id
       book.destroy
+
+      flash[:notice] = "投稿を削除しました"
+      redirect_to root_path
+    else
+
     end
   end
 
