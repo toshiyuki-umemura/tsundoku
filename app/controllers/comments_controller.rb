55ac9960711399c0ae53063c3b9b409094c80442
chokobo-ume <@users.noreply.github.com>
2020-02-12 23:53:13 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,12 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    Comment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    flash[:notice] = "コメントを削除しました"
+    redirect_to root_path
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:content, :book_id).merge(book_id: params[:book_id])
