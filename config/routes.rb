de1cf34527323e0d001202f4118802206de1d962
chokobo-ume <@users.noreply.github.com>
2020-02-12 23:53:18 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
   resources :books do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
   end
   resources :users, only: [:show]
 end
