c8f0e5c9176306a699e6ef440fce0d704ca1a887
chokobo-ume <@users.noreply.github.com>
2020-02-23 20:12:41 +0900

    add_guest-login

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:show]
   resources :tags, param: :tag, only: [:show]
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 end
