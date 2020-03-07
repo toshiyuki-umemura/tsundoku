fda8b0e9dcc928ac1ac3dff3dd6c14f2ccec10bd
chokobo-ume <@users.noreply.github.com>
2020-03-07 18:26:10 +0900

    add user-model_test_email-error

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -18,6 +18,12 @@ describe User do
       user.valid?
       expect(user.errors[:nickname]).to include("を入力してください")
     end
+
+    it "emailが空では登録できないこと" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("が入力されていません。")
+    end
     
   end
 end
