7665d03ed60db3b462d78ee5a4dee959e631b470
chokobo-ume <@users.noreply.github.com>
2020-03-08 00:34:43 +0900

    change_to_faker factory_user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,9 +3,9 @@ FactoryBot.define do
   password = Faker::Internet.password(min_length: 6)
 
   factory :user do
-    name                  {"aiueo"}
-    nickname              {"aaiiuuee"}
-    email                 {"kkk@gmail.com"}
+    name                  {Faker::Name.name}
+    nickname              {Faker::Name.initials(number: 8)}
+    email                 {Faker::Internet.email}
     password              { password }
     password_confirmation { password }
   end
