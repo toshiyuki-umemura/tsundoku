1b4fb904b2cd3089f8a995ce9e0699be34cda3be
chokobo-ume <@users.noreply.github.com>
2020-03-07 18:04:48 +0900

    add_factory_bot-setting_user

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    name                  {"aiueo"}
+    nickname              {"aaiiuuee"}
+    email                 {"kkk@gmail.com"}
+    password              {"000000"}
+    password_confirmation {"000000"}
+  end
+end
