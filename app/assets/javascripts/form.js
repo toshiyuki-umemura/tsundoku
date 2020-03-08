cfb1e106741934293c72eff7c6468be8b2aeec8c
chokobo-ume <@users.noreply.github.com>
2020-03-08 21:03:06 +0900

    edit_word-count

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,3 +1,8 @@
+$(window).on('load', function (){
+  let countNum = String($("#input-text").val().length);
+  $("#counter").text("現在の入力文字数：" + countNum + "文字");
+});
+
 $(function(){
   $("#input-text").on("keyup", function() {
     let countNum = String($(this).val().length);
