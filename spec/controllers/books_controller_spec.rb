58514a9866fbb2f1c0b1a89a1103aefc9b51b6e2
chokobo-ume <@users.noreply.github.com>
2020-03-08 23:55:38 +0900

    add test_new

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -4,36 +4,22 @@ describe BooksController, type: :controller do
 
   let(:user) { create(:user) }
   let(:book) { create(:book, user: user) }
+  let(:books) { create_list(:book, 3, user: user) }
 
   describe 'GET #new' do
+    before { get :new }
+
     it "renders the :new template" do
       get :new
       expect(response).to render_template :new
     end
-  end
 
-  describe 'GET #edit' do
-    it "assigns the requested book to @book" do
-      get :edit, params: { id: book }
-      expect(assigns(:book)).to eq book
+    it 'has a 200 status code' do
+      expect(response).to have_http_status(:ok)
     end
 
-    it "renders the :edit template" do
-      get :edit, params: { id: book }
-      expect(response).to render_template :edit
-    end
-  end
-
-  describe 'GET #index' do
-    it "populates an array of books ordered by created_at DESC" do
-      books = create_list(:book, 3, user: user)
-      get :index
-      expect(assigns(:books)).to match(books.sort{ |a, b| b.created_at <=> a.created_at } )
-    end
-
-    it "renders the :index template" do
-      get :index
-      expect(response).to render_template :index
+    it 'assigns new @book' do
+      expect(assigns(:book)).to be_a_new Book
     end
   end
 
