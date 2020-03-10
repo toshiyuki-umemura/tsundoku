9ccd9166d5e5faa05bb46407e441788c5b0e8a3b
chokobo-ume <@users.noreply.github.com>
2020-03-10 22:26:29 +0900

    add user_model twitter-auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,38 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
 
   has_many :books
   has_many :comments
 
   validates :name,     presence: true
   validates :nickname, presence: true, uniqueness: true
+
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        name: auth.info.name,
+        nickname: auth.info.nickname,
+        email:    User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+
+    user
+  end
+
+  private
+
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
+
+
 
   def self.guest
     user = find_or_create_by!(name: "test", nickname: "guest_user", email: 'guest_user@examaple.com') do |user|
