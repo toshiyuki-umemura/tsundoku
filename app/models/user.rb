d88c43fc6c84e1aa829d8ecd138a03310c45f2ef
chokobo-ume <@users.noreply.github.com>
2020-03-13 01:35:08 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:twitter]
 
   has_many :books
   has_many :comments
