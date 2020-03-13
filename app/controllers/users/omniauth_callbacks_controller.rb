f29c85c75573f9856c7f4086fb2713eee8b0d766
chokobo-ume <@users.noreply.github.com>
2020-03-14 00:34:38 +0900

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -36,7 +36,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to new_user_registration_url
+      redirect_to root_path
     end
   end
 end
