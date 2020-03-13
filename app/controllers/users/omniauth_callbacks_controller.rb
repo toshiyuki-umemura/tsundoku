65d5d3ebcef9e36dda823a66086e2135290d31d0
chokobo-ume <@users.noreply.github.com>
2020-03-13 23:52:48 +0900

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,44 +4,27 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     callback_from :twitter
   end
 
-  # private
-
-  # def callback_from(provider)
-  #   provider = provider.to_s
-
-  #   session[:oauth] = request.env['omniauth.auth'].except('extra')
-  #   session[:user] = User.find_for_oauth(request.env['omniauth.auth'])
-  #   if session[:user]
-  #     flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Twitter'
-  #     sign_in_and_redirect session[:user], event: :authentication
-  #   else
-  #     session["devise.#{provider}_data"] = request.env['omniauth.auth']
-  #     redirect_to new_user_registration_path
-  #   end
-  # end
-
-  # def after_sign_in_path_for(resource)
-  #   if session[:user].id == nil
-  #     new_user_registration_path
-  #   else
-  #     root_path
-  #   end
-  # end
-
   private
+
   def callback_from(provider)
     provider = provider.to_s
 
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-
-    if @user.persisted?
-      print("persisted true")
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-      sign_in_and_redirect @user, event: :authentication
+    session[:oauth] = request.env['omniauth.auth'].except('extra')
+    session[:user] = User.find_for_oauth(request.env['omniauth.auth'])
+    if session[:user]
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Twitter'
+      sign_in_and_redirect session[:user], event: :authentication
     else
-      print("persisted false")
       session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      redirect_to controller: 'sessions', action: 'new'
+      redirect_to new_user_registration_path
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    if session[:user].id == nil
+      new_user_registration_path
+    else
+      root_path
     end
   end
 
