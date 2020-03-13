ea94075d767e9ae1ead2074d7342702b8df7fe71
chokobo-ume <@users.noreply.github.com>
2020-03-14 00:06:20 +0900

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,27 +9,23 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def callback_from(provider)
     provider = provider.to_s
 
-    # session[:user] = User.find_for_oauth(request.env['omniauth.auth'])
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if @user.persisted?
-    # session[:user]
-      print("persisted true")
+    session[:oauth] = request.env['omniauth.auth'].except('extra')
+    session[:user] = User.find_for_oauth(request.env['omniauth.auth'])
+    if session[:user]
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Twitter'
-      # sign_in_and_redirect session[:user], event: :authentication
-      sign_in_and_redirect @user, event: :authentication
+      sign_in_and_redirect session[:user], event: :authentication
     else
-      print("persisted false")
       session["devise.#{provider}_data"] = request.env['omniauth.auth']
       redirect_to new_user_registration_path
     end
   end
 
-  # def after_sign_in_path_for(resource)
-  #   if session[:user].id == nil
-  #     new_user_registration_path
-  #   else
-  #     root_path
-  #   end
-  # end
+  def after_sign_in_path_for(resource)
+    if session[:user].id == nil
+      new_user_registration_path
+    else
+      root_path
+    end
+  end
 
 end
