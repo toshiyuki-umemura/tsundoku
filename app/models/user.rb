68986eddd47229c30454042975020bd87832df36
chokobo-ume <@users.noreply.github.com>
2020-03-14 00:30:32 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,29 +10,52 @@ class User < ApplicationRecord
   validates :name,     presence: true
   validates :nickname, presence: true, uniqueness: true, length: { maximum: 8 }
 
-  def self.find_for_oauth(auth)
-    user = User.where(uid: auth.uid, provider: auth.provider).first
+  # def self.find_for_oauth(auth)
+  #   user = User.where(uid: auth.uid, provider: auth.provider).first
 
+  #   unless user
+  #     user = User.create(
+  #       uid:      auth.uid,
+  #       provider: auth.provider,
+  #       name: auth.info.name,
+  #       nickname: auth.info.nickname,
+  #       email:    User.dummy_email(auth),
+  #       password: Devise.friendly_token[0, 20]
+  #     )
+  #   end
+
+  #   user
+  # end
+
+  # private
+
+  # def self.dummy_email(auth)
+  #   "#{auth.uid}-#{auth.provider}@example.com"
+  # end
+
+  def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
     unless user
-      user = User.create(
-        uid:      auth.uid,
-        provider: auth.provider,
-        name: auth.info.name,
-        nickname: auth.info.nickname,
-        email:    User.dummy_email(auth),
-        password: Devise.friendly_token[0, 20]
-      )
+      user = User.create(name:     auth.info.name,
+                         nickname: auth.info.nickname,
+                         provider: auth.provider,
+                         uid:      auth.uid,
+                         email:    User.create_unique_email,
+                         password: Devise.friendly_token[0,20]
+                        )
     end
-
     user
   end
 
-  private
-
-  def self.dummy_email(auth)
-    "#{auth.uid}-#{auth.provider}@example.com"
+  # 通常サインアップ時のuid用、Twitter OAuth認証時のemail用にuuidな文字列を生成
+  def self.create_unique_string
+    SecureRandom.uuid
   end
 
+  # twitterではemailを取得できないので、適当に一意のemailを生成
+  def self.create_unique_email
+    User.create_unique_string + "@example.com"
+  end
 
 
   def self.guest
