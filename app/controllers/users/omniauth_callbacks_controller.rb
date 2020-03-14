b9c8020335b029e613e80875c905b74997c4d807
chokobo-ume <@users.noreply.github.com>
2020-03-14 15:16:23 +0900

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,11 +9,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def callback_from(provider)
     provider = provider.to_s
 
-    session[:oauth] = request.env['omniauth.auth'].except('extra')
-    session[:user] = User.find_for_oauth(request.env['omniauth.auth'])
-    if session[:user]
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Twitter'
-      sign_in_and_redirect session[:user], event: :authentication
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
     else
       session["devise.#{provider}_data"] = request.env['omniauth.auth']
       redirect_to new_user_registration_path
@@ -21,7 +20,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def after_sign_in_path_for(resource)
-    if session[:user].id == nil
+    if @user.id == nil
       new_user_registration_path
     else
       root_path
