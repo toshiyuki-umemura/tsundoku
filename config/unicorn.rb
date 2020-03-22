4e963503c4602eb36aceb058cf625a26b5908420
chokobo-ume <@users.noreply.github.com>
2020-03-22 22:24:04 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,36 +1,37 @@
-#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく 
-app_path = File.expand_path('../../', __FILE__)
+#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
+app_path = File.expand_path('../../../', __FILE__)
 
-#アプリケーションサーバの性能を決定する 
+#アプリケーションサーバの性能を決定する
 worker_processes 1
 
-#アプリケーションの設置されているディレクトリを指定 
-working_directory app_path
+#アプリケーションの設置されているディレクトリを指定
+working_directory "#{app_path}/current"
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/tmp/pids/unicorn.pid"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
-#ポート番号を指定 
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+#ポート番号を指定
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
-#エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/log/unicorn.stderr.log"
+#エラーのログを記録するファイルを指定
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 
-#通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+#通常のログを記録するファイルを指定
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
-#Railsアプリケーションの応答を待つ上限時間を設定 
+#Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
 
-#以下は応用的な設定なので説明は割愛 
+#以下は応用的な設定なので説明は割愛
+
 preload_app true
-GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true 
+GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
 
 check_client_connection false
 
 run_once = true
 
-before_fork do |server, worker| 
+before_fork do |server, worker|
   defined?(ActiveRecord::Base) &&
     ActiveRecord::Base.connection.disconnect!
 
