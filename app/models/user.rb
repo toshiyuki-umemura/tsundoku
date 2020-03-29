8104040febb8ee9682c5db4f870d12960f7bee18
chokobo-ume <@users.noreply.github.com>
2020-03-29 20:59:12 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-        #  :omniauthable
+         :recoverable, :rememberable, :validatable, :omniauthable
 
   has_many :books
   has_many :comments
@@ -11,28 +10,28 @@ class User < ApplicationRecord
   validates :name,     presence: true
   validates :nickname, presence: true, uniqueness: true, length: { maximum: 8 }
 
-  # def self.find_for_oauth(auth)
-  #   user = User.where(uid: auth.uid, provider: auth.provider).first
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
 
-  #   unless user
-  #     user = User.create(
-  #       uid:      auth.uid,
-  #       provider: auth.provider,
-  #       name: auth.info.name,
-  #       nickname: auth.info.nickname,
-  #       email:    User.dummy_email(auth),
-  #       password: Devise.friendly_token[0, 20]
-  #     )
-  #   end
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        name: auth.info.name,
+        nickname: auth.info.nickname,
+        email:    User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20]
+      )
+    end
 
-  #   user
-  # end
+    user
+  end
 
-  # private
+  private
 
-  # def self.dummy_email(auth)
-  #   "#{auth.uid}-#{auth.provider}@example.com"
-  # end
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
 
 
 
