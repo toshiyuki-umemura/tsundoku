f93ecded1391045e16a6aa24d1b3d517b8a80115
chokobo-ume <@users.noreply.github.com>
2020-03-29 21:13:31 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
 
-  has_many :books
-  has_many :comments
+  has_many :books, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :name,     presence: true
   validates :nickname, presence: true, uniqueness: true, length: { maximum: 8 }
