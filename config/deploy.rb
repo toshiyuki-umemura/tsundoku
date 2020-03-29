b6235d852b96b6cce74ed806278d152e5fe4524f
chokobo-ume <@users.noreply.github.com>
2020-03-29 20:50:19 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/tsundoku_2.pem']
+                  keys: ['~/.ssh/tsundoku.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
